b2f15a7f8c46e8eff30ebc2b5a37506e06e557a5
Christophe PAGES <christophe.pages@bnpparibas.com>
2013-02-09 23:30:28 +0100

    Add doc in INSTALL

--- gbridge_srv/src/stack.c
+++ gbridge_srv/src/stack.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2003 Eric Lacombe
+  Copyright 2008 Christophe PAGES
   
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
